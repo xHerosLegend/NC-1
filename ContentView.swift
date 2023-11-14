9c8c6e3a1730cd0fcfb305f7a172e66e6efc2db7
xHero <loergame@gmail.com>
2023-11-14 11:54:52 +0100

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NC-1
+//
+//  Created by Carmine Andreozzi on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("PORCO DIO")
+                .bold()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC_1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC_1App.swift
+//  NC-1
+//
+//  Created by Carmine Andreozzi on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NC_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
